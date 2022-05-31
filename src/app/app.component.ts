5bd2319ad3f4cb653b7b7726c278a19d399cf1e1
Salinthip <salinthip.1811@mail.kmutt.ac.th>
2022-05-31 11:00:24 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { OrderDialogComponent } from './order-dialog/order-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
 })
 export class AppComponent {
   title = 'order-management-system';
+  constructor(private dialog: MatDialog){}
+
+  openOrderDialog() {
+    this.dialog.open(OrderDialogComponent, {width: '30%'});
+  }
 }
+
+
+
